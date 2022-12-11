e976781828a69ddecaec15318836ebfa7ec8740e
ram-rimal <ram.rimal@dhunitech.io>
2022-12-11 10:13:03 +0545

    layout page added for the project

--- pages/posts/first-posts.js
+++ pages/posts/first-posts.js
@@ -1,12 +1,18 @@
 import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h1>
         Back to the <Link href="/">Home page</Link>
       </h1>
-    </>
+    </Layout>
   );
 }
